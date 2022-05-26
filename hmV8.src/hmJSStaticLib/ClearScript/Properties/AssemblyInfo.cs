9231f9b1994ff3e2fc01284c2de42860d7e60341
komiyamma <komiyama0629@gmail.com>
2022-05-26 16:54:54 +0900

    秀丸スターユニコードのデコードについては、秀丸掲示板でのアルゴリズムにもとづいて変更

--- hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
+++ hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.9.4.1")]
-[assembly: AssemblyFileVersion("1.9.4.1")]
+[assembly: AssemblyVersion("1.9.4.2")]
+[assembly: AssemblyFileVersion("1.9.4.2")]
