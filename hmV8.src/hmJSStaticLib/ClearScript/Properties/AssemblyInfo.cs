8607bfdf602f627ac47861a448182067e4b36878
komiyamma <komiyama0629@gmail.com>
2022-08-06 17:50:11 +0900

    ClearScript7.2.5ベースへ

--- hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
+++ hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.9.4.4")]
-[assembly: AssemblyFileVersion("1.9.4.4")]
+[assembly: AssemblyVersion("1.9.4.5")]
+[assembly: AssemblyFileVersion("1.9.4.5")]
