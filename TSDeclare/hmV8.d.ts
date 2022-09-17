0c35f88789288a7fed87c24429b036d7d2619f7e
komiyamma <komiyama0629@gmail.com>
2022-09-17 23:48:22 +0900

    コミット

--- TSDeclare/hmV8.d.ts
+++ TSDeclare/hmV8.d.ts
@@ -1,56 +1,56 @@
 declare var clr: any;
- 
+
 declare namespace host {
     function lib(libname: string): any;
     function lib(libns: any, libname: string): any;
     function newComObj(progID: string, serverName?: string): any;
 }
- 
- 
- 
+
+
+
 declare namespace hm {
- 
+
     /**
      * 秀丸のバージョン。
      */
     const version: number;
- 
+
     /**
      * デバッグモニタへの出力。console.logと同じ。
      */
     function debuginfo(message?: any, ...optionalParams: any[]): void;
- 
+
     namespace File {
         interface IEncoding {
             readonly HmEncode: number;
             readonly MsCodePage: number;
         }
- 
+
         interface IHidemaruStreamReader {
             readonly FilePath: string;
             readonly Encoding: IEncoding;
             Read(): string;
             Close(): void;
         }
- 
+
         /**
          * 対象の外部ファイルを秀丸で「はじめて」オープンした際、あなたの設定の秀丸だとどのような文字コードだと判定するか
          */
         function GetEncoding(filepath: string): IEncoding;
- 
+
         /**
          * 対象の外部ファイルを秀丸で「はじめて」オープンした際、あなたの設定の秀丸で開いた時と同じ状態でオープン
          */
         function Open(filepath: string, hm_encode?: number): IHidemaruStreamReader;
     }
- 
+
     namespace Macro {
- 
+
         /**
          * 複数の秀丸マクロをヒアドキュメント的に記述するための特殊な関数表記の型。
          */
         type TFnExpression = (f: any) => void;
- 
+
         /**
          * 秀丸マクロを実行。
          */
@@ -61,7 +61,7 @@ declare namespace hm {
                 readonly Message: string;
             }
         };
- 
+
         /**
          * 秀丸マクロの変数の読み書きのための型。関数のような表記と連想配列のような表記。
          */
@@ -70,14 +70,14 @@ declare namespace hm {
             (key: string, value: any): void;
             [key: string]: any;
         }
- 
+
         /**
          * 秀丸マクロの変数の読み書き。
          */
         let Var: TVar;
 
         interface IStatementResult {
-            Result :number;
+            Result: number;
             Args: object[];
             Error: { readonly Message: string };
             Message: string;
@@ -88,7 +88,7 @@ declare namespace hm {
         let Statement: any;
 
         interface IFunctionResult {
-            Result :number | string;
+            Result: number | string;
             Args: object[];
             Error: { readonly Message: string };
             Message: string;
@@ -96,26 +96,114 @@ declare namespace hm {
         /**
          * 関数の実行
          */
-         let Function: any;
+        let Function: any;
+
+        namespace Flags {
+            namespace Encode {
+                const Sjis: number;
+                const Utf16: number;
+                const Euc: number;
+                const Jis: number;
+                const Utf7: number;
+                const Utf8: number;
+                const Utf16_be: number;
+                const Euro: number;
+                const Gb2312: number;
+                const Big5: number;
+                const Euckr: number;
+                const Johab: number;
+                const Easteuro: number;
+                const Baltic: number;
+                const Greek: number;
+                const Russian: number;
+                const Symbol: number;
+                const Turkish: number;
+                const Hebrew: number;
+                const Arabic: number;
+                const Thai: number;
+                const Vietnamese: number;
+                const Mac: number;
+                const Oem: number;
+                const Default: number;
+                const Utf32: number;
+                const Utf32_be: number;
+                const Binary: number;
+                const LF: number;
+                const CR: number;
+
+                const Bom: number;
+                const NoBom: number;
+                const Selection: number;
+
+                const NoAddHist: number;
+                const WS: number;
+                const WB: number;
+            }
+
+            namespace SearchOption {
+                const Word: number;
+                const Casesense: number;
+                const NoCasesense: number;
+                const Regular: number;
+                const NoRegular: number;
+                const Fuzzy: number;
+                const Hilight: number;
+                const NoHilight: number;
+                const LinkNext: number;
+                const Loop: number;
+
+                const MaskComment: number;
+                const MaskIfdef: number;
+                const MaskNormal: number;
+                const MaskScript: number;
+                const MaskString: number;
+                const MaskTag: number;
+                const MaskOnly: number;
+                const FEnableMaskFlags: number;
+
+                const FEnableReplace: number;
+                const Ask: number;
+                const NoClose: number;
+
+                const SubDir: number;
+                const Icon: number;
+                const Filelist: number;
+                const FullPath: number;
+                const OutputSingle: number;
+                const OutputSameTab: number;
+
+                const BackUp: number;
+                const Preview: number;
+
+                const FEnableSearchOption2: number;
+            }
+            namespace SearchOption2 {
+                const UnMatch: number;
+                const InColorMarker: number;
+                const FGrepFormColumn: number;
+                const FGrepFormHitOnly: number;
+                const FGrepFormSortDate: number;
+            }
+        }
     }
- 
+
     namespace Edit {
- 
+
         /**
          * 編集中のテキスト内容の読み書き
          */
         let TotalText: string;
- 
+
         /**
          * 通常選択や行選択のテキスト内容の読み書き
          */
         let SelectedText: string;
- 
+
         /**
          * カーソル行のテキスト内容の読み書き
          */
         let LineText: string;
- 
+
         /**
          * カーソルの位置
          */
@@ -123,7 +211,7 @@ declare namespace hm {
             readonly column: number;
             readonly lineno: number;
         };
- 
+
         /**
          * マウスの位置に対応するカーソルの位置
          */
@@ -136,36 +224,36 @@ declare namespace hm {
     }
 
     namespace OutputPane {
- 
+
         /**
          * アウトプット枠への出力
          */
-         function Output(message: string): number;
- 
+        function Output(message: string): number;
+
         /**
          * 一時退避
          */
-         function Push(): number;
+        function Push(): number;
 
         /**
          * 復元
          */
-         function Pop(): number;
+        function Pop(): number;
 
-         /**
-         * クリア
-         */
-          function Clear(): number;
+        /**
+        * クリア
+        */
+        function Clear(): number;
 
-         /**
-         * アウトプット枠へのコマンド実行
-         */
-          function SendMessage(command_id: number): number;
+        /**
+        * アウトプット枠へのコマンド実行
+        */
+        function SendMessage(command_id: number): number;
 
-         /**
-         * ベースディレクトリの設定
-         */
-          function SetBaseDir(dirpath: string): number;
+        /**
+        * ベースディレクトリの設定
+        */
+        function SetBaseDir(dirpath: string): number;
     }
 
     namespace ExplorerPane {
@@ -173,48 +261,48 @@ declare namespace hm {
         /**
          * ファイルマネージャ枠のモードの設定
          */
-         function SetMode(mode: number): number;
- 
+        function SetMode(mode: number): number;
+
         /**
          * ファイルマネージャ枠のモードの取得
          */
-         function GetMode(): number;
+        function GetMode(): number;
 
         /**
          * ファイルマネージャ枠に指定のファイルのプロジェクトを読み込む
          */
-         function LoadProject(filename: string): number;
+        function LoadProject(filename: string): number;
 
         /**
          * ファイルマネージャ枠のプロジェクトを指定ファイルに保存
          */
-         function SaveProject(filename: string): number;
+        function SaveProject(filename: string): number;
 
         /**
          * ファイルマネージャ枠にプロジェクトを読み込んでいるならば、そのファイルパスを取得する(読み込んでいなければNoneが返る)
          */
-         function GetProject(): string;
+        function GetProject(): string;
 
         /**
          * ファイルマネージャ枠にメッセージを送る
          */
-         function SendMessage(command_id: number): number;
+        function SendMessage(command_id: number): number;
 
         /**
          * ファイルマネージャ枠が「プロジェクト」表示のとき、更新された状態であるかどうかを返します
          */
-         function GetUpdated(): number;
+        function GetUpdated(): number;
 
         /**
          * ファイルマネージャ枠のカレントディレクトリを返す
          */
-         function GetCurrentDir(): string;
+        function GetCurrentDir(): string;
     }
 }
- 
+
 interface Console {
     assert(test?: boolean, message?: string, ...optionalParams: any[]): void;
     log(message?: any, ...optionalParams: any[]): void;
 }
- 
+
 declare var console: Console;
