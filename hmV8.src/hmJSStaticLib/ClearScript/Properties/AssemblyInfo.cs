d50b94c50a6cde2ca83c36fbb60160043603b1cd
komiyamma <komiyama0629@gmail.com>
2024-03-16 16:52:59 +0900

    部分編集モードで hm.Edit.TotalText に値を設定した場合の不具合を修正

--- hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
+++ hmV8.src/hmJSStaticLib/ClearScript/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.1.1")]
-[assembly: AssemblyFileVersion("2.0.1.1")]
+[assembly: AssemblyVersion("2.0.2.1")]
+[assembly: AssemblyFileVersion("2.0.2.1")]
